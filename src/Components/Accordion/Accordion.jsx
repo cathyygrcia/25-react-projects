0186bb5ecd27fbc88a8bb21cb5c72316015fbd54
Cathy Garcia <garciacathy67@yahoo.com>
2024-07-08 17:06:52 -0700

    created accordion files.

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -1,7 +1,11 @@
+import React, { useState } from "react";
+
 export default function Accordion() {
+  const [selected, setSelected] = useState();
+
   return (
     <>
-      <div>Accordion </div>
+      <div></div>
     </>
   );
 }
