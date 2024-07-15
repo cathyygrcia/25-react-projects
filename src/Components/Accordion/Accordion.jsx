558e8824a50808bb6ee1caf18cfc10b02cb9dbd6
Cathy Garcia <garciacathy67@yahoo.com>
2024-07-15 12:54:46 -0700

    Added some styling using tailwind

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -11,16 +11,18 @@ export default function Accordion() {
   return (
     <>
       <div className="flex justify-center text-xl">
-        <div className="accordion">
+        <div className="accordion w-1/3 mt-8 text-purple-900">
           {data && data.length > 0 ? (
             data.map((dataItem) => (
-              <div className="item mb-2.5">
+              <div className="bg-pink-300 mb-2.5 p-3" key={dataItem.id}>
                 <div
                   onClick={() => handleSingleSelection(dataItem.id)}
-                  className="title text-center"
+                  className="title text-center cursor-pointer flex justify-between"
                 >
                   <h3>{dataItem.question}</h3>
                   <span className="flex justify-center">+</span>
+                </div>
+                <div className="mt-2">
                   {selected === dataItem.id ? (
                     <div>{dataItem.answer}</div>
                   ) : null}
