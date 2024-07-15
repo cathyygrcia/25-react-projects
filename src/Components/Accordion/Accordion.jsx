087336e982ae836fe6096306d2327966fbe5cbf6
Cathy Garcia <garciacathy67@yahoo.com>
2024-07-15 11:32:39 -0700

    mapped over dummy data to display on page

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
+import data from "./data.ts";
 
 export default function Accordion() {
   const [selected, setSelected] = useState();
 
   return (
     <>
-      <div></div>
+      <div className="wrapper">
+        <div className="accordion">
+          {data && data.length > 0 ? (
+            data.map((dataItem) => (
+              <div className="item">
+                <div className="title">
+                  <h3>{dataItem.question}</h3>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div>No data found !</div>
+          )}
+        </div>
+      </div>
     </>
   );
 }
