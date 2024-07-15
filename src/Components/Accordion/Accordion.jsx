c85da2dbdce7833f83ef0468e6b3f232bbdaded4
Cathy Garcia <garciacathy67@yahoo.com>
2024-07-15 12:17:27 -0700

    created the handleclick function to display the item that was selected

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -4,15 +4,23 @@ import data from "./data.ts";
 export default function Accordion() {
   const [selected, setSelected] = useState();
 
+  function handleSingleSelection(currentId) {
+    console.log(currentId);
+  }
+
   return (
     <>
-      <div className="wrapper">
+      <div className="flex justify-center text-xl">
         <div className="accordion">
           {data && data.length > 0 ? (
             data.map((dataItem) => (
-              <div className="item">
-                <div className="title">
+              <div className="item mb-2.5">
+                <div
+                  onClick={() => handleSingleSelection(dataItem.id)}
+                  className="title text-center"
+                >
                   <h3>{dataItem.question}</h3>
+                  <span className="flex justify-center">+</span>
                 </div>
               </div>
             ))
