795c2049e809aa4d330ab42732f0272d7f592cd4
Cathy Garcia <garciacathy12@gmail.com>
2024-07-29 11:46:02 -0700

    Created a new folder and file for the start rating project. Also added react-icons.

---
+++ src/Components/Star Rating/StarRating.jsx
@@ -0,0 +1,11 @@
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating() {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center">
+        <FaStar className="text-6xl " />
+      </div>
+    </>
+  );
+}
